7f1410cc4cb67a4e5e69f9c29aa28b58a9389e02
rayhanramin <55429573+rayhanramin@users.noreply.github.com>
2020-07-09 02:35:23 -0500

    Capable of extracting fixing commit from a bugzilla issue page.

--- vulnerabilitiesfirefox.py
+++ vulnerabilitiesfirefox.py
@@ -3,14 +3,34 @@ import re
 from bs4 import BeautifulSoup
 import pprint
 
-URL = "https://bugzilla.mozilla.org/show_bug.cgi?id=1504452"
+#a sample web link from the bugzilla website(A list of bugs for firefox is available in our github repo.)
+URL = "https://bugzilla.mozilla.org/show_bug.cgi?id=1444506"
 
 page = requests.get(URL)
 #pprint.pprint(page.content)
 
+if page.status_code != 200:
+    print('Can not retrieve the requested url %s', URL)
+    exit(1)
+
 soup = BeautifulSoup(page.content,'html.parser')
+
+#separate the comments fromt the other html element
 comments = soup.find_all('div', class_ ='change-set')
 
-
 for i in comments:
-    print(i.text.strip())
+    chng = i.find('div',class_ ='activity')
+
+    if chng is None:
+        continue
+    else:
+        pre = i.find('pre', class_='comment-text')
+        changeset = chng.find_all('div', class_ = 'change')
+        for j in changeset:
+            ser = j.text.strip()
+            pat = re.compile("Resolution: --- → FIXED")
+            if pat.search(ser):
+                links = pre.find_all('a')
+                #print(pre)
+                for z in links:
+                    print(z.text.strip())
