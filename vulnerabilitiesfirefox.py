a2632b711c574dba8fc424ed242aea84a994efac
rayhanramin <55429573+rayhanramin@users.noreply.github.com>
2020-07-10 00:43:58 -0500

    New files added

--- vulnerabilitiesfirefox.py
+++ vulnerabilitiesfirefox.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 import pprint
 
 #a sample web link from the bugzilla website(A list of bugs for firefox is available in our github repo.)
-URL = "https://bugzilla.mozilla.org/show_bug.cgi?id=1444506"
+URL = "https://bugzilla.mozilla.org/show_bug.cgi?id=1000185"
 
 page = requests.get(URL)
 #pprint.pprint(page.content)
@@ -26,12 +26,18 @@ for i in comments:
         continue
     else:
         pre_tag = i.find('pre', class_='comment-text')
+        markdown_tag = i.find('div', class_ = 'comment-text markdown-body')
         changeset_tag = activity_tag.find_all('div', class_ = 'change')
         for j in changeset_tag:
             main_text = j.text.strip()
-            patter = re.compile("Resolution: --- → FIXED")
-            if patter.search(main_text):
-                links = pre_tag.find_all('a')
-                #print(pre)
-                for z in links:
-                    print(z.text.strip())
+            pattern = re.compile("Resolution: --- → FIXED")
+            if pattern.search(main_text):
+                if markdown_tag is None:
+                    links = pre_tag.find_all('a')
+                    #print(pre)
+                    for z in links:
+                        print(z.text.strip())
+                else:
+                    new_links = markdown_tag.find_all('a')
+                    for z in new_links:
+                        print(z.text.strip())
